f16e31ed0d99b2b3e55dfba160e6cedd9779c1dd
Alex Baranov <>
2020-01-01 17:23:26 -0500

    login and register containers

--- laravel-react-login/resources/js/bootstrap.js
+++ laravel-react-login/resources/js/bootstrap.js
@@ -1,5 +1,13 @@
 window._ = require('lodash');
 
+
+try {
+    window.Popper = require("popper.js").default;
+    window.$ = window.jQuery = require("jquery");
+
+    require("bootstrap");
+} catch (e) {}
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -9,6 +17,14 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+} else {
+    console.error(
+        "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
+    );
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
